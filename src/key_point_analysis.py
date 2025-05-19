c683475195e494157f37328f8a6d2f638d084e14
noy-sternlicht <noy.sternlicht@mail.huji.ac.il>
2025-05-19 10:42:24 +0300

    Update KPA analysis code

--- src/key_point_analysis.py
+++ src/key_point_analysis.py
@@ -26,57 +26,85 @@ def main():
     judge_results['keypoints'] = judge_results['comment_id'].apply(
         lambda x: keypoints_per_comment[x] if x in keypoints_per_comment else [])
 
-    judge_results['unique_keypoints'] = judge_results['keypoints'].apply(lambda x: list(set(x)))
 
-    keypoints_to_nr_comments = {}
+    keypoints_count = {}
     for _, row in judge_results.iterrows():
-        for keypoint in row['unique_keypoints']:
-            if keypoint not in keypoints_to_nr_comments:
-                keypoints_to_nr_comments[keypoint] = 0
-            keypoints_to_nr_comments[keypoint] += 1
-
-    keypoints_to_nr_comments_list = []
-    for keypoint, count in keypoints_to_nr_comments.items():
-        keypoints_to_nr_comments_list.append({
+        for keypoint in row['keypoints']:
+            if keypoint not in keypoints_count:
+                keypoints_count[keypoint] = 0
+            keypoints_count[keypoint] += 1
+    keypoints_count_list = []
+    total_keypoints = sum(keypoints_count.values())
+    for keypoint, count in keypoints_count.items():
+        keypoints_count_list.append({
             'keypoint': keypoint,
-            '# speeches': count,
             'stance': kp_stence[keypoint],
+            '#': count,
+            '%': count / total_keypoints * 100,
         })
 
-    keypoints_to_nr_comments_df = pd.DataFrame(keypoints_to_nr_comments_list)
-    keypoints_to_nr_comments_df = keypoints_to_nr_comments_df.sort_values(by=['stance', '# speeches'], ascending=[True, False])
-    keypoints_to_nr_comments_df['% speeches'] = keypoints_to_nr_comments_df['# speeches'] / len(judge_results) * 100
-    keypoints_to_nr_comments_df = keypoints_to_nr_comments_df.round(2)
-    keypoints_to_nr_comments_df_path = os.path.join(args.output_path, 'keypoints_to_nr_comments.csv')
-    keypoints_to_nr_comments_df.to_csv(keypoints_to_nr_comments_df_path, index=False)
-    logger.info(f'Wrote keypoints to nr comments to {keypoints_to_nr_comments_df_path}')
 
-    keypoints_to_nr_source_comments = {}
-    for _, row in judge_results.iterrows():
-        for keypoint in row['unique_keypoints']:
-            if keypoint not in keypoints_to_nr_source_comments:
-                keypoints_to_nr_source_comments[keypoint] = {}
-            if row['source'] not in keypoints_to_nr_source_comments[keypoint]:
-                keypoints_to_nr_source_comments[keypoint][row['source']] = 0
-            keypoints_to_nr_source_comments[keypoint][row['source']] += 1
+    keypoints_count_df = pd.DataFrame(keypoints_count_list)
 
-    keypoints_to_nr_source_comments_list = []
-    for keypoint, sources in keypoints_to_nr_source_comments.items():
-        row = {'keypoint': keypoint, 'stance': kp_stence[keypoint]}
-        all_speeches = 0
-        for source, count in sources.items():
-            row[f'% {source}'] = count / len(judge_results[judge_results['source'] == source]) * 100
-            all_speeches += count
-        row['% speeches'] = all_speeches / len(judge_results) * 100
-        keypoints_to_nr_source_comments_list.append(row)
+    top_3_pros = keypoints_count_df[keypoints_count_df['stance'] == 'pro'].nlargest(3, '%')
+    top_3_cons = keypoints_count_df[keypoints_count_df['stance'] == 'con'].nlargest(3, '%')
 
-    keypoints_to_nr_source_comments_df = pd.DataFrame(keypoints_to_nr_source_comments_list)
-    keypoints_to_nr_source_comments_df = keypoints_to_nr_source_comments_df.sort_values(by=['stance', 'keypoint'], ascending=[True, True])
-    keypoints_to_nr_source_comments_df = keypoints_to_nr_source_comments_df.round(2)
-    keypoints_to_nr_source_comments_df_path = os.path.join(args.output_path, 'keypoints_to_nr_source_comments.csv')
-    keypoints_to_nr_source_comments_df.to_csv(keypoints_to_nr_source_comments_df_path, index=False)
-    logger.info(f'Wrote keypoints to nr source comments to {keypoints_to_nr_source_comments_df_path}')
+    keypoints_per_source = {}
+    unique_sources = judge_results['source'].unique()
+    for source in unique_sources:
+        source_comments = judge_results[judge_results['source'] == source]
+        keypoints_per_source[source] = {}
+        for _, row in source_comments.iterrows():
+            for keypoint in row['keypoints']:
+                if keypoint in top_3_pros['keypoint'].values or keypoint in top_3_cons['keypoint'].values:
+                    if keypoint not in keypoints_per_source[source]:
+                        keypoints_per_source[source][keypoint] = 0
+                    keypoints_per_source[source][keypoint] += 1
+                else:
+                    if kp_stence[keypoint] == 'pro':
+                        if 'Other (pro)' not in keypoints_per_source[source]:
+                            keypoints_per_source[source]['Other (pro)'] = 0
+                        keypoints_per_source[source]['Other (pro)'] += 1
+                    else:
+                        if 'Other (con)' not in keypoints_per_source[source]:
+                            keypoints_per_source[source]['Other (con)'] = 0
+                        keypoints_per_source[source]['Other (con)'] += 1
 
+    keypoints_per_source_list = []
+    for source, keypoints in keypoints_per_source.items():
+        nr_source_keypoints = sum(keypoints.values())
+        for keypoint, count in keypoints.items():
+            stance = 'pro'
+            if (keypoint == 'Other (con)') or (keypoint != 'Other (pro)' and kp_stence[keypoint] == 'con'):
+                stance = 'con'
+            keypoints_per_source_list.append({
+                'source': source,
+                'keypoint': keypoint,
+                'stance': stance,
+                '#': count,
+                '%': count / nr_source_keypoints * 100,
+            })
+    keypoints_per_source_df = pd.DataFrame(keypoints_per_source_list)
+
+    unique_keypoints = keypoints_per_source_df['keypoint'].unique()
+    keypoints_to_source_stats = []
+    for keypoint in unique_keypoints:
+        row = {'keypoint': keypoint}
+        for source in unique_sources:
+            source_data = keypoints_per_source_df[keypoints_per_source_df['source'] == source]
+            source_keypoints = source_data[source_data['keypoint'] == keypoint]
+            if len(source_keypoints) > 0:
+                row[source] = source_keypoints['%'].values[0]
+            else:
+                row[source] = 0
+        keypoints_to_source_stats.append(row)
+    keypoints_to_source_stats_df = pd.DataFrame(keypoints_to_source_stats)
+    keypoints_to_source_stats_df = keypoints_to_source_stats_df.set_index(['keypoint'])
+    keypoints_to_source_stats_df = keypoints_to_source_stats_df.fillna(0)
+    keypoints_to_source_stats_df = keypoints_to_source_stats_df.round(2).T
+    keypoints_to_source_stats_df_path = os.path.join(args.output_path, 'keypoints_to_source_stats.csv')
+    keypoints_to_source_stats_df.to_csv(keypoints_to_source_stats_df_path)
+    logger.info(f'Wrote keypoints to source stats to {keypoints_to_source_stats_df_path}')
 
 
 if __name__ == '__main__':
